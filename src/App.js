c3bde5ba12b74df7c72f8ee817d6adf8f2447a24
Manoj Pillamari <pillamarimanoj@gmail.com>
2024-01-08 15:55:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { nanoid } from 'nanoid';
 
 function App() {
+  const [notes, setNotes] = useState([])
+  const [newTask, setNewTask] = useState('')
+
+  const handleAddTask = () => {
+    if (newTask !== "") {
+      setNotes([...notes, { id: nanoid(), task: newTask }])
+      setNewTask('')
+    }
+    else {
+      alert("Task cannot be empty")
+    }
+  }
+
+  const handleDeleteTask = (noteID) => {
+    setNotes((prevData)=>prevData.filter((task)=>task.id !== noteID))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ textAlign: "center", margin: "1%" }}>Simple To-Do Application</h1>
+      <label htmlFor='taskDetail'>Enter New Task Here</label><br />
+      <input type='text' name='taskDetail' placeholder='Add a new task' value={newTask} onChange={(e) => { setNewTask(e.target.value) }} /><br />
+      <button onClick={handleAddTask}>Add</button>
+      {notes.map((notesData) => {
+        return (
+          <ul key={notesData.id} style={{display:'flex', gap:"1%"}}>
+            <li>{notesData.task}</li>
+            <div onClick={()=>handleDeleteTask(notesData.id)}>🗑️</div>
+          </ul>)
+      })}
     </div>
   );
 }
