0218c99f65776343685dd1187074b523b3c95d71
Manoj Pillamari <pillamarimanoj@gmail.com>
2024-01-10 11:40:39 +0530

    Added table instead of list for TASKS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { useState } from 'react';
 import './App.css';
 import { nanoid } from 'nanoid';
+import { BsTrashFill } from 'react-icons/bs';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Table } from 'react-bootstrap';
 
 function App() {
   const [notes, setNotes] = useState([])
   const [newTask, setNewTask] = useState('')
 
-  const handleAddTask = () => {
+  const handleAddTask = (e) => {
+    e.preventDefault()
     if (newTask !== "") {
       setNotes([...notes, { id: nanoid(), task: newTask }])
       setNewTask('')
@@ -16,23 +20,46 @@ function App() {
     }
   }
 
-  const handleDeleteTask = (noteID) => {
-    setNotes((prevData)=>prevData.filter((task)=>task.id !== noteID))
+  const handleDeleteTask = (e, noteID) => {
+    e.preventDefault()
+    setNotes((prevData) => prevData.filter((task) => task.id !== noteID))
   }
 
   return (
     <div className="App">
-      <h1 style={{ textAlign: "center", margin: "1%" }}>Simple To-Do Application</h1>
+      <h1 style={{ textAlign: "center", margin: "1%", textDecoration: "underline" }}>Simple To-Do Application</h1>
       <label htmlFor='taskDetail'>Enter New Task Here</label><br />
-      <input type='text' name='taskDetail' placeholder='Add a new task' value={newTask} onChange={(e) => { setNewTask(e.target.value) }} /><br />
-      <button onClick={handleAddTask}>Add</button>
-      {notes.map((notesData) => {
-        return (
-          <ul key={notesData.id} style={{display:'flex', gap:"1%"}}>
-            <li>{notesData.task}</li>
-            <div onClick={()=>handleDeleteTask(notesData.id)}>🗑️</div>
-          </ul>)
-      })}
+      <textarea className="new-task-data" name='taskDetail' placeholder='Add a new task' value={newTask} onChange={(e) => { setNewTask(e.target.value) }} /><br />
+      <button onClick={(e) => handleAddTask(e)}>Add</button>
+      <div style={{ width: "-webkit-fill-available", padding: "2% 0%" }}>
+        <Table bordered responsive style={{ border: "black" }}>
+          <thead>
+            <tr style={{ textAlign: "center" }}>
+              <th style={{ width: "90", backgroundColor: "#c9c4b5" }}>Task</th>
+              <th style={{ width: "10%", backgroundColor: "#c9c4b5" }}>Action</th>
+            </tr>
+          </thead>
+          {notes.length === 0 ?
+            <tbody>
+              <tr>
+                <td colSpan={2} style={{ textAlign: "center" }}>
+                  There are no Tasks
+                </td>
+              </tr>
+            </tbody>
+            :
+            notes?.map((notesData) => {
+              return (
+                <tbody>
+                  <tr>
+                    <td style={{ width: "90%", overflowWrap: "anywhere" }}>{notesData.task}</td>
+                    <td style={{ width: "10%", textAlign: "center" }}><BsTrashFill size={20} color='red' onClick={(e) => handleDeleteTask(e, notesData.id)} style={{ cursor: "pointer" }} /></td>
+                  </tr>
+                </tbody>)
+            })
+          }
+        </Table>
+      </div>
     </div>
   );
 }
